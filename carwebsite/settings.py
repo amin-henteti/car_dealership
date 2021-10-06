3292410059b518b68462eef4f412c7155eac81db
amin henteti <aminhenteti21@gmail.com>
2021-10-06 04:17:35 +0100

    adding Team database model

--- carwebsite/settings.py
+++ carwebsite/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'carwebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_Car_website',
+        'USER': 'postgres',
+        'PASSWORD': 'amin',
+        'HOST': 'localhost',
     }
 }
 
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Team(models.Model):
+    first_name = models.CharField(max_length=100);
+    last_name = models.CharField(max_length=100);
+    designation = models.CharField(max_length=100);
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d')
+    facebook_link = models.URLField(max_length=100);
+    twitterlink = models.URLField(max_length=100);
+    create_dta = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name #'{0} {1}'.format(self.first_name, self.last_name)
+        
