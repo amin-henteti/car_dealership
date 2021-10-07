b771da0245bbdf96064e5bd36639a5df895c7868
amin henteti <aminhenteti21@gmail.com>
2021-10-07 12:19:26 +0100

    add latout for Member model in Admin side

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from pages.models import Team
-admin.site.register(Team)
+from pages.models import Member
+
+from django.utils.html import format_html
+class MemberAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src ="{}" width=40 style="border-radius: 40px" />'.format(object.photo.url))
+    thumbnail.short_description = 'Photo'
+    list_display = ('id', 'thumbnail', 'first_name', 'last_name', 'ocuppation', 'created_date',)
+    list_display_links = ('id', 'thumbnail', 'first_name',)
+    search_fields = ('first_name', 'last_name', 'ocuppation',)
+    list_filter = ('last_name', 'ocuppation',)
+
+
+
+admin.site.register(Member, MemberAdmin)
