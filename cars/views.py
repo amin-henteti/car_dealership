335102f732d61cddd77fac7ac97f201039f01e7f
amin henteti <aminhenteti21@gmail.com>
2021-10-20 21:28:54 +0100

    custom admin side

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,4 @@
+from io import SEEK_SET
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models.query import QuerySet
@@ -14,23 +15,26 @@ data = {
     "nav_list": ("HOME", "CARS", "SERVICES", "ABOUT", "CONTACT"),
 }
 
+home_search_fields = ("title", "model", "location", "year")
+fields_values = {
+    field: Car.objects.values_list(field, flat=True).distinct()
+    for field in home_search_fields
+}
+
 
 def gat_views_images(all_cars):
     views_images = {}
     for car in all_cars:
-        views_images[
-            car.id
-        ] = (
-            []
-        )  # initialize the views_image of a certain car indexed by a the id in the database
+        # initialize the views_image of a certain car indexed by a the id in the database
+        views_images[car.id] = []
         for i in range(MAX_NUMBER_VIEWS):
             attr = "views_images_" + str(i + 1)  # 0 is for the main image in a way
             view = getattr(car, attr)
             try:
-                url = view.url  # get the url of an image otherwise throw an error
-                if isinstance(
-                    url, str
-                ):  # make sure the url is well defined and not empty or none
+                # get the url of an image otherwise throw an error
+                url = view.url
+                # make sure the url is well defined and not empty or none
+                if isinstance(url, str):
                     views_images[car.id].append(url)
             except AttributeError as e:
                 print(e)
@@ -50,26 +54,25 @@ def cars(request):
     data2 = {
         "all_cars": paged_cars,
         "views_images": views_images,
+        "search_fields": fields_values,
     }
     return render(request, "cars/cars.html", {**data, **data2})
 
 
 def car_details(request, id):
     car = get_object_or_404(Car, pk=id)  # Car.objects.get(id=id)
-    car_views_images = {
-        "carousel-selector-0": car.main_image.url
-    }  # initialize the views_image of the car
+    # initialize the views_image of the car
+    car_views_images = {"carousel-selector-0": car.main_image.url}
     for i in range(MAX_NUMBER_VIEWS):
         attr = "views_images_" + str(i + 1)  # 0 is reserved for the main image
         view = getattr(car, attr)
         try:
-            url = view.url  # get the url of an image otherwise throw an error
-            if isinstance(
-                url, str
-            ):  # make sure the url is well defined and not empty or none
-                car_views_images[
-                    "carousel-selector-" + str(i + 1)
-                ] = url  # need this odd looking key for that my version of jinja cant set a variable
+            # get the url of an image otherwise throw an error
+            url = view.url
+            # make sure the url is well defined and not empty or none
+            if isinstance(url, str):
+                # need this odd looking key for that my version of jinja cant set a variable
+                car_views_images["carousel-selector-" + str(i + 1)] = url
         except AttributeError as e:
             print(e)
         except ValueError as ee:
@@ -85,35 +88,48 @@ def car_details(request, id):
 def search(request):
 
     all_cars = Car.objects.order_by("-created_date")
-
-    keyword = request.GET.get("keyword")
-
-    if not (keyword is None):
-        filter_cars = all_cars.filter(title__icontains=keyword) # initialisation, QuerySet(Car) doesnt work to make an empty query
-        cars_fields = [
-            "availibility",
-            "body_style",
-            "color",
-            "condition",
-            "description",
-            "engine",
-            "features",
-            "fuel_type",
-            "transmission",
-            "location",
-            "model",
-            "provinance",
-            "title",
-            "year",
-        ]
-
-        for field in cars_fields:
-            print(55 * "_")
-            cars_field = {field + "__icontains": keyword}
-            q = all_cars.filter(**cars_field)  # A filter from all cars query
-            filter_cars = filter_cars | q # update the search by joining the query with the previous ones
-            print("Succes ", field, q.__len__, filter_cars.__len__)
-    else:
+    filter_cars = None
+    for field_name in home_search_fields:
+        field_value = request.GET.get(field_name)
+        if not (field_value is None):
+            cars_fields = (
+                [field_name]
+                if field_name != "keyword"
+                else [
+                    "availibility",
+                    "body_style",
+                    "color",
+                    "condition",
+                    "description",
+                    "engine",
+                    "features",
+                    "fuel_type",
+                    "transmission",
+                    "location",
+                    "model",
+                    "provinance",
+                    "title",
+                    "year",
+                ]
+            )
+            if filter_cars is None:
+                cond = {cars_fields[0] + "__icontains": field_value}
+                # initialisation, QuerySet(Car) doesnt work to make an empty query
+                filter_cars = all_cars.filter(**cond)
+            print(55 * "-", cars_fields)
+            # in the keyword case we make search in multiple fields with union condition (OR)
+            if field_name == "keyword":
+                for field in cars_fields:
+                    cond = {field + "__icontains": field_value}
+                    if filter_cars is None:
+                        q = all_cars.filter(**cond)  # A filter from all cars query
+                    # update the search by joining the query with the previous ones
+                    filter_cars = filter_cars | q
+            else:
+                filter_cars = filter_cars.filter(
+                    **{cars_fields[0] + "__icontains": field_value}
+                )
+    if filter_cars is None:  # No applied filters
         filter_cars = all_cars
     paginator = Paginator(filter_cars, CARS_PER_PAGE)
     page = request.GET.get("page")
@@ -123,5 +139,6 @@ def search(request):
     data2 = {
         "all_cars": paged_cars,
         "views_images": views_images,
+        "search_fields": fields_values,
     }
     return render(request, "cars/search.html", {**data, **data2})
--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .models import Member
 from cars.models import Car, MAX_NUMBER_VIEWS
-from cars.views import gat_views_images, data
+from cars.views import gat_views_images, data, fields_values
 
 # Create your views here.
 
@@ -11,12 +11,12 @@ def home(request):
     featured_cars = Car.objects.order_by("-price").filter(availibility="Sale")
     all_cars = Car.objects.order_by("-created_date")
     views_images = gat_views_images(all_cars)
-
     data2 = {
         "featured_cars": featured_cars,
         "all_cars": all_cars,
         "views_images": views_images,
         "members": Member.objects.all(),
+        "search_fields": fields_values,
     }
     return render(request, "pages/home.html", {**data, **data2})
 
