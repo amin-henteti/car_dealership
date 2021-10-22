e014bc559b663478ffc408588ec6e530fff8511d
amin henteti <aminhenteti21@gmail.com>
2021-10-22 08:32:16 +0100

    add file keys

---
+++ secret_keys.py
@@ -0,0 +1,10 @@
+import os
+
+os.environ['EMAIL_HOST_USER'] = 'anonyme0000xx@gmail.com'
+os.environ['EMAIL_HOST_PASSWORD'] = 'anonyme0000@xx2'
+
+os.environ['SECRET_KEY'] = 'django-insecure-_1qv!(sw*ks*@y5jfbf8&9%t92u&a%cepzam3ut1$fzsc!pkiw'
+os.environ['POSTGRES_PASSWORD'] = 'amin'
+
+os.environ['DJANGO_USERNAME'] = 'henteti'
+os.environ['DJANGO_PASSWORD'] = 'aminChu7caex'
